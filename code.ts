5caf31777af2fdfb56c18e95b6d281ef74a9c90d
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2023-12-23 02:50:18 +0400

    feat: upgrade

--- code.ts
+++ code.ts
@@ -1,32 +1,32 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
 function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-const node = figma.currentPage.selection[0];
+const node = figma.currentPage.selection[0]
 
-if (node && "children" in node && node.children.length) {
-  const timeout = 1000 / node.children.length;
-  const width = node.width;
-  const height = node.height;
+if (node && 'children' in node && node.children.length) {
+  const timeout = 1000 / node.children.length
+  const width = node.width
+  const height = node.height
   for (const child of node.children) {
-    child.visible = false;
+    child.visible = false
   }
   node.children.forEach((child, i) => {
     setTimeout(() => {
-      child.x = getRandomInt(width);
-      child.y = getRandomInt(height);
-      child.rotation = getRandomInt(360);
-      child.visible = true;
-    }, i * timeout);
-  });
+      child.x = getRandomInt(width)
+      child.y = getRandomInt(height)
+      if ('rotation' in child) child.rotation = getRandomInt(360)
+      child.visible = true
+    }, i * timeout)
+  })
   setTimeout(() => {
-    figma.closePlugin();
-  }, node.children.length * timeout + 1);
+    figma.closePlugin()
+  }, node.children.length * timeout + 1)
 } else {
-  figma.notify("Please first select a frame with children elements");
-  figma.closePlugin();
+  figma.notify('Please first select a frame with children elements')
+  figma.closePlugin()
 }
